fca4b1b25a01d11baa42e88cce55940eafae3ee2
mendesmoe <m.mendes@depiltech.com>
2023-11-27 17:30:58 +0100

    feat:token-authentication

--- src/main/java/doctolib/com/api/controller/AuthController.java
+++ src/main/java/doctolib/com/api/controller/AuthController.java
@@ -1,8 +1,8 @@
 package doctolib.com.api.controller;
 
-import doctolib.com.api.domain.user.AuthService;
 import doctolib.com.api.domain.user.DataAuth;
 import doctolib.com.api.domain.user.User;
+import doctolib.com.api.infra.security.DataTokenJWT;
 import doctolib.com.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +26,16 @@ public class AuthController {
 
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid DataAuth data) {
-        var token = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var auth = manager.authenticate(token);
-        var authentication = manager.authenticate(token);
-        return ResponseEntity.ok(tokenService.newToken((User) authentication.getPrincipal()));
+        try {
+            var authToken = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+            var authentication = manager.authenticate(authToken);
+            var token = tokenService.newToken((User) authentication.getPrincipal());
+
+            return ResponseEntity.ok(new DataTokenJWT(token));
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
